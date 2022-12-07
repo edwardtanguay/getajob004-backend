975ae986421c3c0666a5955b338772af09895fe3
Edward Tanguay (windows) <edward@tanguay.info>
2022-12-07 11:23:13 +0100

    jobs have empty skills

--- src/model.ts
+++ src/model.ts
@@ -1,5 +1,12 @@
 import fs from 'fs';
 
+type Skill = {
+	idCode: string;
+	name: string;
+	url: string;
+	description: string;
+}
+
 type Job = {
 	id: number,
 	title: string,
@@ -7,17 +14,27 @@ type Job = {
 	url: string,
 	description: string,
 	skillList: string,
+	skills: Skill[]
 	todo: string,
 }
 
-const jobs = (JSON.parse(fs.readFileSync('./src/data/jobs.json', 'utf8')) as Job[]);
+const _jobs: any[] = JSON.parse(fs.readFileSync('./src/data/jobs.json', 'utf8'));
+const skillInfos: any = (JSON.parse(fs.readFileSync('./src/data/skillInfos.json', 'utf8')));
 
 export const getJobs = () => {
+	const jobs: Job[] = [];
+	_jobs.forEach((_job: any) => {
+		const job: Job = {
+			..._job,
+			skills: []
+		};
+		jobs.push(job);
+	})
 	return jobs;
 }
 
 export const getTodos = () => {
-	return jobs.map((job: Job) => {
+	return _jobs.map((job: Job) => {
 		return {
 			todo: job.todo,
 			company: job.company,
